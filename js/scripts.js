1936424164acab7e09f39cc18c3170c594ee3516
ahmad.nrohim <ahmad.nrohim@gmail.com>
2023-06-08 15:35:32 +0700

    feature: add mini coding project to feature branch in github

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+function validateInput() {
+  var name = document.forms["message-form"]["nama"].value;
+  var birthDate = document.forms["message-form"]["tanggal-lahir"].value;
+  var gender = document.forms["message-form"]["jenis-kelamin"].value;
+  var message = document.forms["message-form"]["pesan"].value;
+
+  if (name == "" || birthDate == "" || gender == "" || message == "") {
+    alert("Input tidak boleh kosong");
+    return false;
+  }
+
+  setOutput(name, birthDate, gender, message);
+  return false;
+}
+
+function setOutput(name, birthDate, gender, message) {
+  document.getElementById("output-current-time").innerText = new Date();
+  document.getElementById("output-nama").innerText = name;
+  document.getElementById("output-tanggal-lahir").innerText = birthDate;
+  document.getElementById("output-jenis-kelamin").innerText = gender;
+  document.getElementById("output-pesan").innerText = message;
+  return false;
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
